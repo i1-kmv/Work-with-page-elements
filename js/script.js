3c35216bed8873fa7150f64f0c68ae0000ad33c7
i1-kmv <i1-kmv@mail.ru>
2020-06-13 16:44:53 +0200

    step1

--- js/script.js
+++ js/script.js
@@ -10,3 +10,26 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img');
+
+adv.forEach(item=> {
+    item.remove();
+});
+
+const genre = document.querySelector('.promo__genre');
+genre.textContent = 'драма';
+
+const poster = document.querySelector('.promo__bg');
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+const movieList = document.querySelector('.promo__interactive-list');
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += ` 
+        <li class="promo__interactive-item">${i + 1}. ${film}
+              <div class="delete"></div>
+        </li>`;
+});
